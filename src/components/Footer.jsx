42d3a98fdd3455bec2c24d81d6930de585e82d3f
chriistinapark <162581412+chriistinapark@users.noreply.github.com>
2025-05-20 16:55:15 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-dark text-light text-center py-3 mt-auto">
       Пак Кристина и Елисеева Варвара | © 2025 |
-      <a className="text-info ms-1" href="https://github.com/username/meal-spa" target="_blank" rel="noreferrer">Repo</a>
+      <a className="text-info ms-1" href="https://chriistinapark.github.io/meal-spa/" target="_blank" rel="noreferrer">Repo</a>
     </footer>
   );
 }
